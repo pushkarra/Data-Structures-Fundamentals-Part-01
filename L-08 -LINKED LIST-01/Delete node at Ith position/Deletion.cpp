9f3fc99ea01878aa9db3697b858d11ed003683ba
Pushkar Rajput <pushkarpuru97199@gmail.com>
2022-06-07 20:45:48 +0530

    Simple Deletion of node in a linked list

--- L-08 -LINKED LIST-01/Delete node at Ith position/Deletion.cpp
+++ L-08 -LINKED LIST-01/Delete node at Ith position/Deletion.cpp
@@ -1,63 +1,149 @@
-@csalgo
 
-Coding Problem
-Problem Statement: Delete a Node from Linked List
-Problem Level: MEDIUM
-Problem Description:
-You have been given a linked list of integers. Your task is to write a function that deletes a node from a given position, 'pos'.
-Note :
-Assume that the Indexing for the linked list always starts from 0.
+// Problem Statement: Delete a Node from Linked List
+// Problem Level: MEDIUM
+// Problem Description:
+// You have been given a linked list of integers. Your task is to write a function that deletes a node from a given position, 'pos'.
+// Note :
+// Assume that the Indexing for the linked list always starts from 0.
 
-If the position is greater than or equal to the length of the linked list, you should return the same linked list without any change.
+// If the position is greater than or equal to the length of the linked list, you should return the same linked list without any change.
 
-Illustration :
-The following images depict how the deletion has been performed.
+// Illustration :
+// The following images depict how the deletion has been performed.
 
-Image-I :
+// Image-I :
 
-Image-II :
+// Image-II :
 
-Input format :
-The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+// Input format :
+// The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
 
-The first line of each test case or query contains the elements of the linked list separated by a single space.
+// The first line of each test case or query contains the elements of the linked list separated by a single space.
 
-The second line of each test case contains the integer value of 'pos'. It denotes the position in the linked list from where the node has to be deleted.
+// The second line of each test case contains the integer value of 'pos'. It denotes the position in the linked list from where the node has to be deleted.
 
-Remember/Consider :
-While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+// Remember/Consider :
+// While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
 
-Output format :
-For each test case/query, print the resulting linked list of integers in a row, separated by a single space.
+// Output format :
+// For each test case/query, print the resulting linked list of integers in a row, separated by a single space.
 
-Output for every test case will be printed in a seperate line.
-You don't need to print explicitly, it has been taken care of.
+// Output for every test case will be printed in a seperate line.
+// You don't need to print explicitly, it has been taken care of.
 
-Constraints :
-1 <= t <= 10^2
-0 <= N <= 10^5
-pos >= 0
-Time Limit: 1sec
+// Constraints :
+// 1 <= t <= 10^2
+// 0 <= N <= 10^5
+// pos >= 0
+// Time Limit: 1sec
 
-Where 'N' is the size of the singly linked list.
+// Where 'N' is the size of the singly linked list.
 
-Sample Input 1 :
-1 
-3 4 5 2 6 1 9 -1
-3
+// Sample Input 1 :
+// 1 
+// 3 4 5 2 6 1 9 -1
+// 3
 
-Sample Output 1 :
-3 4 5 6 1 9
+// Sample Output 1 :
+// 3 4 5 6 1 9
 
-Sample Input 2 :
-2
-3 4 5 2 6 1 9 -1
-0
-10 20 30 40 50 60 -1
-7
+// Sample Input 2 :
+// 2
+// 3 4 5 2 6 1 9 -1
+// 0
+// 10 20 30 40 50 60 -1
+// 7
 
-Sample Output 2 :
-4 5 2 6 1 9
-10 20 30 40 50 60
+// Sample Output 2 :
+// 4 5 2 6 1 9
+// 10 20 30 40 50 60
 
-@csalgo
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+	public:
+		int data;
+		Node *next;           
+		
+	Node(int data){           
+		this ->data = data;
+		next = NULL;
+	}
+};
+
+
+Node* modifiedGetInput(){                      
+	int data;
+	cin>>data;
+	
+	Node *head = NULL;
+	Node *tail = NULL;
+	
+	while(data != -1){
+		Node *newNode = new Node(data); 
+		if(head == NULL){               
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin>>data;                     
+	}
+	
+	return head;                       
+}
+
+
+Node *deleteNode(Node *head, int index){
+	
+	Node *temp = head;
+	int count = 0;
+	
+	if(index == 0){                  // SPECIAL CASE FOR DELETION AT INDEX 0
+		head = head->next;
+		return head;
+	}	
+	
+	while(count < index-1 && temp != NULL){    // CHECKPOST FOR WRONG INDEX ENTERED
+		temp = temp->next;
+		count++;
+	}
+	
+	if(temp != NULL){              // MAIN PART FOR INDEX OTHER THAN 0
+	   Node *a = temp -> next;
+	   Node *b = a -> next;
+	
+	   temp -> next = b;
+	   delete a;	
+	}
+	
+	return head;
+}
+
+
+
+void print (Node *head){
+	Node *temp = head;       
+	while(temp != NULL){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+}
+
+
+int main(){
+	
+	int index;
+	Node *head = modifiedGetInput();
+	print(head);
+	cout<<endl;
+	cin>>index;
+	head = deleteNode(head,index);
+	print(head);
+}
